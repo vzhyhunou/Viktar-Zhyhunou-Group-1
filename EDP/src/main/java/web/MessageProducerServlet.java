5baa3f55b8682b0f7b5b23828d512b674324eed1
Aliaksandr Darafeyeu <Aliaksandr_Darafeyeu@epam.com>
2015-02-11 14:27:45 +0300

    EDP task

--- EDP/src/main/java/web/MessageProducerServlet.java
+++ EDP/src/main/java/web/MessageProducerServlet.java
@@ -27,31 +27,19 @@ import java.util.Date;
 @WebServlet(description = "EnterpriseDesignPatternServlet", urlPatterns = {"/EDPServlet", "/EDPServlet.do"})
 public class MessageProducerServlet extends HttpServlet {
 
-    Context ic = null;
     XAResource xaRes1;
     XAConnection xaConn1;
     Connection conn1;
     MysqlXADataSource mysqlXADataSource;
 
     public void init() throws ServletException {
-
         super.init();
-
         mysqlXADataSource = new MysqlXADataSource();
         mysqlXADataSource.setUrl("jdbc:mysql://localhost:3306/mashdb");
         mysqlXADataSource.setPinGlobalTxToPhysicalConnection(true);
         mysqlXADataSource.setRollbackOnPooledClose(true);
         mysqlXADataSource.setUser("root");
         mysqlXADataSource.setPassword("ttt");
-
-        try {
-            xaConn1 = mysqlXADataSource.getXAConnection();
-            xaRes1 = xaConn1.getXAResource();
-            conn1 = xaConn1.getConnection();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -63,19 +51,22 @@ public class MessageProducerServlet extends HttpServlet {
         //prepare audit data
         String user = mysqlXADataSource.getUser();
         Date date = new Date();
-
         try {
 
-            //2 phase commit example
+            xaConn1 = mysqlXADataSource.getXAConnection();
+            xaRes1 = xaConn1.getXAResource();
+            conn1 = xaConn1.getConnection();
+
+
             xaRes1.start(xid, XAResource.TMNOFLAGS);
             conn1.createStatement().executeUpdate("insert into " +
-                    "mashdb.ticket values (null,'terminator',100,'" + user + '/' + date + "')");
+                "mashdb.ticket values (null,'terminator',100,'" + user + '/' + date + "')");
             xaRes1.end(xid, XAResource.TMSUCCESS);
 
             //second transaction branch joins to first
             xaRes1.start(xid, XAResource.TMJOIN);
             conn1.createStatement().executeUpdate("insert into " +
-                    "mashdb.ticket values (null,'titanic',120,'" + user + '/' + date + "')");
+                "mashdb.ticket values (null,'titanic',120,'" + user + '/' + date + "')");
             xaRes1.end(xid, XAResource.TMSUCCESS);
 
             xaRes1.prepare(xid);
@@ -86,30 +77,30 @@ public class MessageProducerServlet extends HttpServlet {
             e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+
+            if (xaConn1 != null) {
+                try {
+                    xaConn1.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (conn1 != null) {
+                try {
+                    conn1.close();
+                } catch (SQLException e) {
+                   e.printStackTrace();
+                }
+            }
         }
+
 
         request.getRequestDispatcher("WEB-INF/jsps/input.jsp").forward(request, response);
     }
 
-
-    @Override
-    protected void finalize() throws Throwable {
-        super.finalize();
-        if (xaConn1 != null) {
-            xaConn1.close();
-        }
-        if (conn1 != null) {
-           conn1.close();
-        }
-    }
-
     private static Context getContext() throws NamingException {
-//        Hashtable<String, Object> p = new Hashtable<String, Object>();
-//        p.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
         final Context context = new InitialContext();
         return context;
     }
-
-
-
 }
