d74cb2dab24e3854323bb7fe34b5dc3e685e4e27
Aliaksandr Darafeyeu <aliaksandr.darafeyeu@ihg.com>
2016-10-20 00:30:46 +0300

    added spaces

--- BankModel/src/main/java/com/epam/darafeyeu/dao/PersonDAO.java
+++ BankModel/src/main/java/com/epam/darafeyeu/dao/PersonDAO.java
@@ -10,7 +10,7 @@ import java.util.List;
  */
 public interface PersonDAO {
 
-    public List<Person> getAllPersons();
+    public List<Person> getAllPersons()  ;
 
     public Person getPerson(Integer personId) throws PersonNotFoundException;
 
